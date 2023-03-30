db903042eb97660b61a2eb45da6c57855c64c1fc
“MinhNguyen0306” <minhnguyen183385@gmail.com>
2023-03-30 21:12:31 +0700

    push app

---
+++ server/src/controllers/review.controller.js
@@ -0,0 +1,54 @@
+import responseHandler from "../handlers/response.handler.js";
+import reviewModel from "../models/review.model.js";
+
+const create = async (req, res) => {
+    try {
+        const { movieId } = req.params
+
+        const review = new reviewModel({
+            user: req.user.id,
+            movieId,
+            ...req.body
+        })
+
+        await review.save()
+
+        responseHandler.created(res, {
+            ...review._doc,
+            id: review.id,
+            user: req.user 
+        });
+    } catch (error) {
+        responseHandler.error(res)
+    }
+};
+
+const remove = async (req, res) => {
+    try {
+        const { reviewId } = req.params;
+
+        const review = await reviewModel.findOne({ user: req.user.id, _id: reviewId })
+
+        if(!review) return responseHandler.notFound(res)
+
+        await review.remove()
+
+        responseHandler.ok(res, review)
+    } catch (error) {
+        responseHandler.error(res)
+    }
+}
+
+const getReviewsOfUser = async (req, res) => {
+    try {
+        const reviews = await reviewModel.find({
+            user: req.user.id,
+        }).sort("-createAt")
+
+        responseHandler.ok(res, reviews)
+    } catch (error) {
+        responseHandler.error(res)
+    }
+};
+
+export default { create, remove, getReviewsOfUser };
